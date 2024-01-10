85991c81d30f838a1950c6aa15af819bd1bcb100
Upendra <upendrabr.93@gmail.com>
2024-01-10 12:59:04 +0530

    Optimization

--- src/components/Introduction/Introduction.js
+++ src/components/Introduction/Introduction.js
@@ -7,6 +7,10 @@ import pricing from '../../assets/images/pricing.png';
 import milestone from '../../assets/images/milestone.png';
 import certificationImg from "../../assets/images/certification-bro.svg";
 import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import './Introduction.css'
 
 const Introduction = () => {
     const companyName = 'Gayathri\'s company';
@@ -16,7 +20,35 @@ const Introduction = () => {
         {heading: 'Industry experts', subText: 'Seasoned experts h decades of experience across multiple domains', icon: expert},
         {heading: 'Fair Pricing', subText: 'Our solutions are afforable and easy on your pocket', icon: pricing},
         {heading: 'Always Available', subText: 'High quality support. Night or day, rain or snow, we\'ve got your back', icon: callCenter},
+    ];
+
+    const roadmapList = [
+        {heading: 'Assess current security posture'},
+        {heading: 'Perform gap analysis'},
+        {heading: 'Identify critical risks along with business priorities'},
+        {heading: 'Build tailored security roadmap'},
+        {heading: 'Execute, Monitor, Enhance'}
     ]
+
+    const servicesList = [
+        {heading: 'Software Security', icon: callCenter},
+        {heading: 'Infrastructure Security', icon: callCenter},
+        {heading: 'Risk and Compliance', icon: callCenter},
+        {heading: 'Managed IT Services', icon: callCenter},
+        {heading: 'Data Privacy and Security', icon: callCenter},
+        {heading: 'Virtual CISO(vCISO)', icon: callCenter},
+        {heading: 'Managed Cyber SOC', icon: callCenter},
+        {heading: 'Managed Security Services', icon: callCenter}
+    ]
+
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1
+    };
+    
     return (
         <>
             <Box className="flex justify-center items-center flex-col w-full px-10 py-32" 
@@ -38,12 +70,12 @@ const Introduction = () => {
             <div className="px-4 width-full">
                 <div className="text-5xl text-center mt-6">Why choose us? </div>
                 <div className="text-center mb-8">Benefits of engaging with {companyName}</div>
-                <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-4 content-center justify-around lg:px-32">
-                    {benefitsWithUs.map(d => {
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-4 content-center justify-around lg:px-32">
+                    {benefitsWithUs.map((d, i) => {
                         return (
                         <div 
                             className="border rounded-md border-slate-200 flex items-center justify-center flex-col p-6
-                            col-span-1">
+                            col-span-1" key={i}>
                             <img src={d.icon} style={{height: '64px'}} alt=''></img>
                             <div className="text-2xl font-semibold text-center text-gray-700">{d.heading}</div>
                             <div className="text-center text-gray-600">{d.subText}</div>
@@ -59,11 +91,7 @@ const Introduction = () => {
                     <div className="text-5xl text-left mt-6 text-gray-700 mb-4">Get a free roadmap to secure your business in 5 days </div>
                     <div className="text-left text-gray-600">We help in building a customized security program to meet your business needs and align with the industry best practices. We try to identify your risk and where you should focus your efforts using some of the best security practices and frameworks.</div>
                     <ul >
-                        <li className="my-2"><CheckBoxIcon sx={{color: '#1583e7'}}/>Assess current security posture</li>
-                        <li className="my-2"><CheckBoxIcon sx={{color: '#1583e7'}}/>Perform gap analysis</li>
-                        <li className="my-2"><CheckBoxIcon sx={{color: '#1583e7'}}/>Identify critical risks along with business priorities</li>
-                        <li className="my-2"><CheckBoxIcon sx={{color: '#1583e7'}}/>Build tailored security roadmap</li>
-                        <li className="my-2"><CheckBoxIcon sx={{color: '#1583e7'}}/>Execute, Monitor, Enhance</li>
+                        {roadmapList.map((d, i) => <li className="my-2"><CheckBoxIcon sx={{color: '#1583e7'}}/>{d.heading}</li>)}
                     </ul>
                 </div>
             </div>
@@ -72,7 +100,7 @@ const Introduction = () => {
                     <input className="border rounded-md h-10 w-[300px] px-5" placeholder="Enter  here" type="hidden" name="access key" value={API_ACCESS_KEY} />
                     <input className="border rounded-md h-10 w-[300px] px-5" placeholder="Enter Name here" type="text" name="name" required />
                     <input className="border rounded-md h-10 w-[300px] px-5" placeholder="Enter Email here" type="email" name="email" required />
-                    <div class="h-captcha" data-captcha="true"></div>
+                    <div className="h-captcha" data-captcha="true"></div>
                     <Button variant="contained" sx={{height: '2.5rem', width: '200px'}} type="submit">Submit Form</Button>
                 </form>
             </div>
@@ -90,7 +118,19 @@ const Introduction = () => {
             <div className="flex flex-col justify-center items-center p-16 ">
                 <div className="text-3xl font-semibold text-gray-600">Choose Your Right Solution</div>
                 <div className="text-xl text-gray-600">We understand that every business is unique. That is why we collaborate with you to create cybersecurity solutions that complement your business while also meeting your specific security requirements.</div>
-                <div className="font-semibold text-xl">At CYBERNERDS, we provide tailored services for every business, including:</div>
+                <div className="font-semibold text-xl">I provide tailored services for every business, including:</div>
+            </div>
+            <div className="sliderContainer p-16">
+                <Slider {...settings}>
+                    {servicesList.map(d => {
+                        return <div>
+                            <div className="flex flex-col items-center justify-center border mx-3 p-10">
+                                <img src={d.icon} alt={d.heading} />
+                                <h3>{d.heading}</h3>
+                            </div>
+                        </div>
+                    })}
+                </Slider>
             </div>
 
 
